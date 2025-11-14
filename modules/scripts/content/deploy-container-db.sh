701fa8750b721d5bf91424edd7eeb2ad4982bbcd
Bongani Masuku <bongani@1702tech.com>
2025-11-14 22:11:14 +0200

    OPH-131 - build: deploy-container-db.sh

---
+++ modules/scripts/content/deploy-container-db.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+#region required variables
+
+# AWS_REGION e.g. 'eu-west-1'
+# LOAD_ENV_VARS_SCRIPT_S3_URL: s3://[bucket-name]/path/to/script.sh
+# AWS_SSM_PARAMETER_PATHS e.g. "path1;path2;..."
+
+#endregion
+
+#region validations
+
+if [[ "$AWS_REGION" == "" ]];then
+  echo "[build-container]: AWS Region not set. please set AWS Region"
+  exit 1
+fi
+
+if [[ "$LOAD_ENV_VARS_SCRIPT_S3_URL" == "" ]];then
+  echo "[build-container]: load-env-vars script AWS S3 URL not set. please set load-env-vars script AWS S3 URL"
+  exit 1
+fi
+
+if [[ "$AWS_SSM_PARAMETER_PATHS" == "" ]];then
+  echo "[build-container]: AWS SSM Parameter Store path(s) not set. please set AWS SSM Parameter Store path(s)"
+  exit 1
+fi
+
+#endregion
+
+LOAD_ENV_VARS_SCRIPT_PATH=./ci/load-env-vars.sh
+aws s3 cp $LOAD_ENV_VARS_SCRIPT_S3_URL $LOAD_ENV_VARS_SCRIPT_PATH
+
+source $LOAD_ENV_VARS_SCRIPT_PATH $AWS_REGION $AWS_SSM_PARAMETER_PATHS $(pwd)
+
+source release-manifest && \
+  docker pull $DKR_IMAGE && \
+  docker run --env-file $LOAD_ENV_VARS_SCRIPT_PATH $DKR_IMAGE
+
+echo "[deploy-db]: done."
