baf92bae0d0e5aa87c878de607046b4952c48fcc
Bongani Masuku <bongani@1702tech.com>
2025-11-14 22:23:42 +0200

    OPH-131 - refactor deploy-cloud.sh

--- modules/scripts/content/deploy-cloud.sh
+++ modules/scripts/content/deploy-cloud.sh
@@ -4,19 +4,43 @@ set -euo pipefail
 
 #region required variables
 
-# ENVIRONMENT_NAME e.g. "prod"
+# AWS_REGION e.g. 'eu-west-1'
+# LOAD_ENV_VARS_SCRIPT_S3_URL: s3://[bucket-name]/path/to/script.sh
+# AWS_SSM_PARAMETER_PATHS e.g. "path1;path2;..."
 # MODULE_DIR e.g. "./module"
 
 #endregion
 
-echo '[deploy-cloud]: starting'
+#region validations
 
-export TF_VAR_ENVIRONMENT_NAME=$ENVIRONMENT_NAME
+if [[ "$AWS_REGION" == "" ]];then
+  echo "[build-container]: AWS Region not set. please set AWS Region"
+  exit 1
+fi
+
+if [[ "$LOAD_ENV_VARS_SCRIPT_S3_URL" == "" ]];then
+  echo "[build-container]: load-env-vars script AWS S3 URL not set. please set load-env-vars script AWS S3 URL"
+  exit 1
+fi
+
+if [[ "$AWS_SSM_PARAMETER_PATHS" == "" ]];then
+  echo "[build-container]: AWS SSM Parameter Store path(s) not set. please set AWS SSM Parameter Store path(s)"
+  exit 1
+fi
 
 if [[ "$MODULE_DIR" == "" ]]; then
   echo "[build-cloud]: terraform child directory is not set, using default: $(pwd)"
   MODULE_DIR=$(pwd)
 fi
+
+#endregion
+
+echo '[deploy-cloud]: starting'
+
+LOAD_ENV_VARS_SCRIPT_PATH=./ci/load-env-vars.sh
+aws s3 cp $LOAD_ENV_VARS_SCRIPT_S3_URL $LOAD_ENV_VARS_SCRIPT_PATH
+
+source $LOAD_ENV_VARS_SCRIPT_PATH $AWS_REGION $AWS_SSM_PARAMETER_PATHS $(pwd)
 
 terraform -chdir=$MODULE_DIR apply tfplan -input=false -auto-approve
 
