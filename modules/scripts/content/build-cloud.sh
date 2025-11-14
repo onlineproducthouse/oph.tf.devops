152c522f1df6a5840a09f0ba201acc5d79ba9a1d
Bongani Masuku <bongani@1702tech.com>
2025-11-14 19:53:14 +0200

    OPH-131 - build build-cloud.sh script

---
+++ modules/scripts/content/build-cloud.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+#region required variables
+
+# ENVIRONMENT_NAME e.g. "prod"
+# MODULE_DIR e.g. "./module"
+
+#endregion
+
+echo '[build-cloud]: starting'
+
+export TF_VAR_ENVIRONMENT_NAME=$ENVIRONMENT_NAME
+
+if [[ -v $MODULE_DIR ]]; then
+  echo "[build-cloud]: terraform child directory set to: $MODULE_DIR"
+else
+  echo "[build-cloud]: terraform child directory is not set, using default: $(pwd)"
+  MODULE_DIR=$(pwd)
+fi
+
+terraform -chdir=$MODULE_DIR init
+terraform -chdir=$MODULE_DIR validate
+terraform -chdir=$MODULE_DIR plan -input=false -out=tfplan
+
+echo '[build-cloud]: done.'
+exit 0
