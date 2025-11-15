32fc9294c985b98b03b2a8d66086ae35a018a500
Bongani Masuku <bongani@1702tech.com>
2025-11-15 14:02:19 +0200

    OPH-131 - build codebuild job module

---
+++ modules/pipelines/job/main.tf
@@ -0,0 +1,71 @@
+resource "aws_security_group" "job" {
+  count = var.vpc_id == "" ? 0 : 1
+
+  name   = "${var.name}-job"
+  vpc_id = var.vpc_id
+
+  lifecycle {
+    create_before_destroy = false
+  }
+}
+
+resource "aws_security_group_rule" "job" {
+  count = var.vpc_id == "" ? 0 : 1
+
+  security_group_id = aws_security_group.job[0].id
+
+  type        = "egress"
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 0
+  to_port     = 0
+}
+
+resource "aws_codebuild_project" "job" {
+  name           = var.name
+  service_role   = var.role_arn
+  build_timeout  = var.timeout
+  queued_timeout = var.timeout
+
+  artifacts {
+    type     = "CODEPIPELINE"
+    location = "CODEPIPELINE"
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = var.buildspec
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    privileged_mode = var.is_container
+
+    dynamic "environment_variable" {
+      for_each = var.env_variables
+
+      content {
+        name  = environment_variable.value.key
+        value = environment_variable.value.value
+      }
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = var.vpc_id == "" ? [] : [{
+      vpc_id             = var.vpc_id
+      subnets            = var.vpc_subnets
+      security_group_ids = [aws_security_group.job[0].id]
+    }]
+
+    content {
+      vpc_id             = vpc_config.value.vpc_id
+      subnets            = vpc_config.value.subnets
+      security_group_ids = vpc_config.value.security_group_ids
+    }
+  }
+}
---
+++ modules/pipelines/job/variables.tf
@@ -0,0 +1,50 @@
+variable "name" {
+  description = "The name given to the codebuild job"
+  type        = string
+  nullable    = false
+}
+
+variable "vpc_id" {
+  description = "VPC where the CI job runs"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_subnets" {
+  description = "VPC Subnets where the CI job runs"
+  type        = list(string)
+  default     = []
+}
+
+variable "role_arn" {
+  description = "The IAM Role ARN the job should run with"
+  type        = string
+}
+
+variable "timeout" {
+  description = "The jobs run timeout"
+  type        = number
+  default     = 5
+}
+
+variable "buildspec" {
+  description = "AWS S3 Bucket ARN key to buildspec object"
+  type        = string
+  nullable    = false
+}
+
+variable "is_container" {
+  description = "Flag whether or not this must run with docker enabled"
+  type        = bool
+  default     = false
+}
+
+variable "env_variables" {
+  description = "A list of environment variables to set in the job instance"
+  default     = []
+
+  type = list(object({
+    key   = string
+    value = string
+  }))
+}
