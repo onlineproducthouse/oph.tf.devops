0fec3e5fcf432ab4af02eca799061bdadde0c3d6
Bongani Masuku <bongani@1702tech.com>
2025-12-31 11:04:44 +0200

    configure skopeo2 provider

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,25 @@
+terraform {
+  required_providers {
+    skopeo2 = {
+      source  = "bsquare-corp/skopeo2"
+      version = "~> 1.1.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "skopeo2" {
+  source {
+    login_username = "my-source-registry-username"
+    login_password = "my-source-registry-password"
+  }
+  destination {
+    login_username = "my-destination-registry-username"
+    login_password = "my-destination-registry-password"
+  }
 }
 
 module "devops_cloud" {
