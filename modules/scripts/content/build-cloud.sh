cb0e96f70eaf0c5b2f0dbc3e381cb58132705e70
Bongani Masuku <bongani@1702tech.com>
2026-01-01 14:50:03 +0200

    cleanup build cloud script

--- modules/scripts/content/build-cloud.sh
+++ modules/scripts/content/build-cloud.sh
@@ -12,14 +12,13 @@ set -euo pipefail
 
 #endregion
 
-apt-get update
+#region install terraform
+apt-get update -y
 apt-get install -y gnupg software-properties-common curl
-
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
 apt-get update && apt-get install terraform
+#endregion
 
 
 #region validations
