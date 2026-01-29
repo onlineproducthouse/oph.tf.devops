88dc5bf557c957ed4f2c31e9e1a8f8b58fb0ab98
Bongani Masuku <bongani@1702tech.com>
2026-01-29 18:01:57 +0200

    cleanup run-test script

--- modules/scripts/content/run-test.sh
+++ modules/scripts/content/run-test.sh
@@ -3,29 +3,30 @@
 set -euo pipefail
 
 #region required variables
-# WORKING_DIR e.g. "./"
 # RUN_TEST_COMMAND e.g. "npm run test"
-
-if [[ "$WORKING_DIR" == "" ]]; then
-  echo "[run-unit-test]: unit tests working directory not set, using default: $(pwd)"
-  WORKING_DIR=$(pwd)
-fi
+# WORKING_DIR e.g. "./"
 
 if [[ "$RUN_TEST_COMMAND" == "" ]]; then
-  echo "[run-unit-test]: unit tests command not set, please set RUN_TEST_COMMAND"
+  echo "[run-test]: tests command not set, please set RUN_TEST_COMMAND"
   exit 1
+fi
+
+if [[ "$WORKING_DIR" == "" ]]; then
+  echo "[run-test]: tests working directory not set, using default: $(pwd)"
+  WORKING_DIR=$(pwd)
 fi
 #endregion
 
-echo '[run-unit-test]: starting'
+echo '[run-test]: starting'
 
 cd $WORKING_DIR
 
 run_test() {
+  echo '[run-test]: executing command - $RUN_TEST_COMMAND'
   $RUN_TEST_COMMAND
 }
 
 run_test
 
-echo '[run-unit-test]: done.'
+echo '[run-test]: done.'
 exit 0
