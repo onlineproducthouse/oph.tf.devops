dbbba808081be576c3033f32baf27c6c6bac65cd
Bongani Masuku <bongani@1702tech.com>
2026-02-01 19:47:48 +0200

    update dependencies

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "role" {
 module "pipeline" {
   source = "./modules/pipeline"
 
-  depends_on = [module.role.*]
+  depends_on = [module.role]
 
   for_each = {
     for v in var.repositories : v.name => v
